cdb0184091c680c3120d3afceb1a016b9e91e7d2
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-08-27 13:10:01 -0700

    Created 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * print numberz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numberz;
+for (numberz = 0; numberz < 10; numberz++)
+putchar(numberz);
+putchar('\n');
+return (0);
+}
