8cd9997a7c060960d83d0afb39660663ddc8fa5c
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-08-27 12:15:57 -0700

    Created 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * print alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i, j;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+for (j = 'A'; j <= 'Z'; i++)
+putchar(j);
+putchar('\n');
+return (0);
+}
