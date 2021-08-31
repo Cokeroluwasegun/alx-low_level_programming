bc2c958a30b36da60cb72ba33872ea89347b0bf9
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-08-31 11:08:35 -0700

    Added prototype to main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * print alphabet - Entry point
- * alphabet.c
+ * print alphabet
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 int i;
 for(i = 'a'; i <= 'z'; i++)
- {
- _putchar(i);
- }
- _putchar('\n');
+{
+_putchar(i);
+}
+_putchar('\n');
 }
