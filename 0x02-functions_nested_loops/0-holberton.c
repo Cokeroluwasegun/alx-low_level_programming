3112d23425a7c08309b8e615aba9ca271590c228
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-08-31 05:11:06 -0700

    Created README.md file,0-holberton.c file and 1-alphabet.c file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
- * main - Entry point
- * 0-holberton.c
- * Return: Always 0 (Success)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 int i;
-char c[] = "Holberton";
-for (i = 0; i <= 8; i++)
+char hb[] = "Holberton";
+for (i = 0; i < 9; i++)
 {
-_putchar(c[i]);
+_putchar(hb[i]);
 }
 _putchar('\n');
 return (0);
