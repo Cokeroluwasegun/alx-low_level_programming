a41e448f8b4d3e1954d786cfc40a19b7ec53f39f
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-08-31 13:02:54 -0700

    Created 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * @i : number to print last digit
+ * Return: int
+*/
+int print_last_digit(int i)
+{
+int x = i % 10;
+if (x < 0)
+x = -x;
+_putchar(x + '0');
+return (x);
+}
