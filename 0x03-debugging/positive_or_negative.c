a174112269baf674cf542f68f3d825da761aed8d
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-01 09:42:05 -0700

    Updated main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * positive_or_negative - Entry point
+ * positive_or_negative
+ * Return: Always 0.
+ */
+void positive_or_negative(int i)
+{
+if (n > 10)
+printf("%d is positive", i);
+else if ( n == 0)
+printf("%d is zero", i);
+else
+printf("%d is negative\n", i);
+}
