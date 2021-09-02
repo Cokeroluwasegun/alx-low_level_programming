fdcf8f93c5efc14d4f6b93a75450e341b92b305b
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-02 00:07:52 -0700

    Created README.md, main.h 0-isupper.c 1-isdigit.c files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isuper - checks for uppercase charcaters
+ *@c: character to check
+ * Return: Always 0 or 1
+ */
+int _isupper(int c)
+{
+if ( c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ This is a README.md file containing details of the project 
