e277fc75a8c2b787e56b0e9d44c89593cbb655fb
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-02 06:06:57 -0700

    Created 2-mul.c and 2-main.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ *@a: multiply a with b to get result
+ *@b: multiply b with a to ge result
+ * Return: Always result.
+ */
+int mul(int a, int b)
+{
+return a * b;
+}
