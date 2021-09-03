25f639fea0fe154c5289f3ac360c6c9d82725964
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-02 21:10:02 -0700

    This was updated to fix errors in main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * main - test function that print if integer is positive
+ * positive or nrgative check
  * Return: 0
  */
 int main(void)
@@ -8,5 +9,4 @@ int main(void)
 int i;
 i = 0;
 positive_or_negative(i);
-return 0;
-}
+return (0);
