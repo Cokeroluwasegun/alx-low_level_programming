169a51850ed5f6b207af7cafe0e7634ea5e44b98
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-06 05:30:55 -0700

    Updated 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * 0-reset_to_98 - reset to 98
- *@n: integer to reset
- * Return: Always 0
+ * @n: integer to reset
+ * Return: void
  */
+/* This is for function */
 void reset_to_98(int *n)
 {
 *n = 98;
