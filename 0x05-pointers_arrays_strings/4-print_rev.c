b2e7da5eddaab870b2211dcd50d2a7aee70c27d3
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-06 07:13:14 -0700

    Created 4-print_rev.c and 4-main.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @str: string to print
+ * Return: void
+ */
+void print_rev(char *str)
+{
+int i = 0;
+for (i = 0; *(str + i); i++)
+for (; i > 0; i--)
+-putchar(*(s + i - 1));
+_putchar('\n');
+}
