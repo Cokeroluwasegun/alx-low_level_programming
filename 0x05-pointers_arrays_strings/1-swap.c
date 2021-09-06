146171731396255dfede7ebe6900d850d74ab9e0
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-06 05:19:25 -0700

    Created 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * 1-swap - swap
+ * @a: first int
+ * @b: second int
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+  int c;
+  c = *a;
+  *a = *b;
+  *b = c;
+}
