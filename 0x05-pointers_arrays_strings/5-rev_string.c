bf735a0ee0bddbb7179f87375538967b1b03f470
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-06 07:51:37 -0700

    Created 5-rev_string.c and 5-main.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int i, j;
+char a;
+for (j = 0; s[j]; j++)
+;
+putchar(j);
+for (i = 0; i <(j / 2); i++)
+{
+a = s[i];
+s[i] = s[j - i -1];
+s[j - i - 1] = a;
+}
+}
