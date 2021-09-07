2175334cc2b2d5b303d0e1ceba57fc8c2f80c71c
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-07 13:59:34 -0700

    This is a commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - 
+ * @s: character
+ * Return: Always 0
+ */
+
+int _atoi(char *s)
+{
+int i = 0;
+int n = 0;
+int sign = 1;
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+{
+if (s[i] == '-')
+sign *= -1;
+i++;
+}
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+{
+if (n >= 0)
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+else
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+}
+sign *= -1;
+}
