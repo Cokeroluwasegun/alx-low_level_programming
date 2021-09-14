5604059af0bc32393bb33db1c8c008edd65f47c9
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-13 20:20:43 -0700

    Created 100-set_string.c and 100-main.c file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* set_string - fills memory with a constant byte.
+* @s: the memory area to print in
+* @to: the memory area to print in
+* Return: void.
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
