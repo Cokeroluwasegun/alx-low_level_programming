7e4b0f5fd9af2142ae368acc666e0864534a654d
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-15 09:18:23 -0700

    Created 5-sqrt_recursion.c and 5-main.c file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*_sqrt_recursion - natural square
+* @n : int
+* Return: int
+*/
+int _squrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return(sqrtchecker(n, (n + 1) / 2));
+}
+/**
+* sqrCheck - test natural square.
+* @a: the memory area to print in
+* @b: to busy for desc :(
+* Return: int.
+*/
+int sqrtchecker(int a, int b)
+{
+if (b * b ==a)
+return (b);
+else if (b == 1)
+return (-1);
+else
+return(sqrtchecker(a, b -1));
+}
