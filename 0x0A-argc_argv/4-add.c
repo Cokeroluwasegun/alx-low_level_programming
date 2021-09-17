ac8d534e7509c83780fb83248df3155cd798794e
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-17 06:07:15 -0700

    Created 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - adds numbers
+* @argc: int
+* @argv: pointer
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+int i, j, x = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (argv[i][j] < 48  || argv[i][j] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+x += atoi(argv[i]);
+}
+printf("%d\n", x);
+return (0);
+}
