2ea8096c76dd053a56a564318999f2670daa77d0
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-17 04:43:10 -0700

    Created 0x0A-argc_argv dir with 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 0-whatsmyname - prints its name followed by a new line
+ * main - prints its name followed by a new line
  * @argc - integer count
  * @argv[] - pointer array
  * Return 0.
@@ -9,6 +9,6 @@
 int main(int argc, char* argv[])
 {
 if(argc >= 0)
-printf("%s\n", argv[]);
+printf("%s\n", argv[0]);
 return (0);
 }
