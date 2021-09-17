a763200317b5dc36aeb0901ab9f35734808c2530
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-17 06:23:52 -0700

    Updated all file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints name of program
- * @argc - integer count
- * @argv - pointer array
- * Return: int
- */
-int main(int argc, char* argv[])
+* main - print name of program
+* @argc: int
+* @argv: pointer
+* Return: int
+*/
+int main(int argc, char *argv[])
 {
-if(argc >= 0)
+if (argc >= 0)
 printf("%s\n", argv[0]);
 return (0);
 }
