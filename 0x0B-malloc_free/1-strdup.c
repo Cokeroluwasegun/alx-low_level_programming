12b6ed484960b6d047b43c76ca939d31cdb4693b
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-21 09:23:29 -0700

    Created 1-strdup.c and 1-main.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* _strdup - copy a string
+* @str: pointer to string
+* Return: pointer
+*/
+char *_strdup(char *str)
+{
+unsigned int i, j;
+char *t;
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i])
+i++;
+t = malloc(sizeof(char) * i + 1);
+if (t == NULL)
+return (NULL);
+for (j = 0 ; j < i; j++)
+t[j] = str[j];
+t[j + 1] = '\0';
+return (t);
+}
