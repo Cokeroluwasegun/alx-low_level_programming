1ecc1303cf7ba48df4241e087daaabddf73ee2ce
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-21 09:13:48 -0700

    Created dir, 0-main and 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* create_array - create an array
+* @size: unsigned int
+* @c: char
+* Return: pointer
+*/
+char *create_array(unsigned int size, char c)
+{
+char *t;
+unsigned int i;
+if (size < 0)
+return (NULL);
+t = malloc(size * sizeof(char));
+if (t == NULL)
+return (NULL);
+for (i = 0 ; i < size; i++)
+*(t + i) = c;
+return (t);
+}
