3de65c0e035c85ff331b343d5e5b20ebe8cd4253
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-21 10:01:31 -0700

    Created 3-alloc_grid.c and 3-main.c file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* alloc_grid- 2d array
+* @width: integer
+* @height: integer
+* Return: pointer
+*/
+int **alloc_grid(int width, int height)
+{
+int **t;
+int i, j, k;
+if ((width <= 0) || (height <= 0))
+return (NULL);
+t = malloc(height * sizeof(int *));
+if (t == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+t[i] = malloc(sizeof(int) * width);
+if (t[i] == NULL)
+{
+for (k = 0; k < i ; k++)
+free(t[k]);
+free(t);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+t[i][j] = 0;
+}
+return (t);
+}
