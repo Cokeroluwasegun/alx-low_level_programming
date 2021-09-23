d00289f953dbc81307fb6c9f8078b3688d0c2627
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-23 10:48:21 -0700

    Created 2-main.c and 2-calloc.c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+* _calloc - concatenate two strings
+* @nmemb: strings
+* @size: int
+* Return: Pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *c;
+if (nmemb <= 0 || size <= 0)
+return (NULL);
+c = malloc(nmemb * size);
+if (c == NULL)
+return (NULL);
+for (i = 0; i < size * nmemb; i++)
+c[i] = 0;
+return (c);
+}
