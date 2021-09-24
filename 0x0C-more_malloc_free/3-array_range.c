8b8750454607002240d2347a371085f2d0316f05
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-24 02:02:05 -0700

    Created 3-main.c and 3-array_range.c file

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+* array_range - create array
+* @min: int
+* @max: int
+* Return: Pointer
+*/
+int *array_range(int min, int max)
+{
+int *c;
+int i, diff;
+if (min > max)
+{
+return (NULL);
+}
+diff = max - min + 1;
+c = malloc(sizeof(int) * (diff));
+if (c == NULL)
+return (NULL);
+for (i = 0; i < diff; i++)
+{
+c[i] = min;
+min++;
+}
+return (c);
+}
