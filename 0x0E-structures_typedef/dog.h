f8265a0f49acb37b3b57a19327e941a5dc9aefaf
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-27 11:54:43 -0700

    Created dir, dog.h and 0-main.c file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Short description
+* @name: name of the dog
+* @age: age of dog
+* @owner: name of owner
+* Description: Longer description
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
