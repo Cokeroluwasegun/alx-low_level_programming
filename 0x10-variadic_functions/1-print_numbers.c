027d03763dd2bd4e54ae5390740bcaad79dfbfd1
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-09-30 14:07:35 -0700

    Created 1-main.c and 1-print_numbers.c file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_numbers - sum of parameters
+* @n: int
+* @separator: a string between numbers
+* Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list al;
+unsigned int i;
+va_start(al, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(al, int));
+if (separator != NULL && i != n - 1)
+printf("%s", separator);
+}
+printf("\n");
+va_end(al);
+}
