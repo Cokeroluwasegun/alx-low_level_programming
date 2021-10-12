b103a76e74a66b04585ea3f860d76ae99b708192
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-10-12 04:38:54 -0700

    Created and updated 0-3 program file

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* add_nodeint - add node at the beginning of a list
+* @head: listint_t
+* @n: integer
+* Return: adress
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *node;
+node = malloc(sizeof(listint_t));
+if (node == NULL)
+return (NULL);
+node->n = n;
+node->next = *head;
+*head = node;
+return (node);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+*add_nodeint_end - add node at the end of a list
+* @head: listint_t
+* @n: integer
+* Return: listint_t
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *node, *last;
+node = malloc(sizeof(listint_t));
+if (node == NULL)
+return (NULL);
+node->n = n;
+node->next = NULL;
+if (*head == NULL)
+{
+*head = node;
+return (node);
+}
+last = *head;
+while (last->next)
+last = last->next;
+last->next = node;
+return (node);
+}
