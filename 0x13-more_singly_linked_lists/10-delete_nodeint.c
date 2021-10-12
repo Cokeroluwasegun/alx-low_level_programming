0834e83e103a2ba42ca07273aedd085b0b544253
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-10-12 05:50:36 -0700

    Created 10-main and 10-delete_nodeint.c file

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* delete_nodeint_at_index - delete node at index
+* @head: listint_t
+* @index: position of the new node to insert
+* Return: integer
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *temp, *aux;
+if (*head == NULL)
+return (-1);
+temp = *head;
+if (index == 0)
+{
+*head = temp->next;
+free(temp);
+return (1);
+}
+index--;
+while (index != 0)
+{
+temp = temp->next;
+if (temp == NULL)
+return (-1);
+index--;
+}
+aux = temp->next;
+temp->next = aux->next;
+free(aux);
+return (1);
+}
