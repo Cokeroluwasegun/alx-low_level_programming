0a0f5d0cf0b31ce44a3eafa4e9a6ae3c1419b4b8
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-10-12 21:25:29 -0700

    Added string.h lib

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 /**
  * print_listint - print all element of a list
  * @h: listint_t
@@ -7,7 +8,7 @@
 size_t print_listint(const listint_t *h)
 {
 size_t i = 0;
-while (h)
+while(h)
 {
 printf("%d\n", h->n);
 h = h->next;
