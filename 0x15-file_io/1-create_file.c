671086644f3447ee54b254c275cf28b0f08781ba
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-10-18 17:44:47 -0700

    Updated file 1 and 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
 /**
- * create_file - reads a text file and prints it to stdout.
- * @filename: file's name.
- * @text_content: number of letters it should read and print.
- * Return: number of characters printed
+ * create_file - Create a file
+ * @filename: file name
+ * @text_content: content of file
+ * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, wr, len;
+int fd, i = 0, w;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 if (text_content == NULL)
 return (1);
-len = 0;
-while (text_content[len])
-len++;
-wr = write(fd, text_content, len);
-if (wr == -1)
+while (text_content[i])
+i++;
+w = write(fd, text_content, i);
+if (w == -1 || w != i)
 return (-1);
-close(fd);
 return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,64 +1,74 @@
-#include<stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include<fcntl.h>
-#include <sys/types.h>
+#include "main.h"
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
-int main(int argc, char **argv)
+ * verif - verif
+ * @fd: fd
+ */
+void verif(int fd)
 {
-int fd1, fd2, rd, wr;
-char buf[1024];
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+/**
+ * print - print
+ * @r1: r1
+ * @buff1: buff1
+ * @fd1: fd1
+ * @fd2: fd2
+ * @argv1: argv1
+ * @argv2: argv2
+ */
+void print(ssize_t r1, char *buff1,
+int fd1, int fd2, char *argv1, char *argv2)
+{
+while ((r1 = read(fd1, buff1, 1024)) > 0)
+{
+if ((write(fd2, buff1, r1)) != r1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv2);
+exit(99);
+}
+}
+if (r1 == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv1);
+exit(98);
+}
+}
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int fd1, fd2, f1, f2;
+ssize_t r1 = 0;
+char buff1[1024];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-if (argv[1] == NULL)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-fd1 = open(argv[1], O_RDWR, 0777);
+fd1 = open(argv[1], O_RDONLY);
 if (fd1 == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-fd2 = open(argv[2],  O_RDWR | O_CREAT | O_TRUNC, 0664);
+fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd2 == -1)
-{
-dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-exit(99);
-}
-rd = read(fd1, buf, 1024);
-while (rd > 0)
-{
-wr = write(fd2, buf, rd);
-if (wr != rd)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-rd = read(fd1, buf, 1024);
-}
-if (rd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-if (close (fd1) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
-exit(100);
-}
-if (close(fd2) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
-exit(100);
-}
+print(r1, buff1, fd1, fd2, argv[1], argv[2]);
+f1 = close(fd1);
+f2 = close(fd2);
+verif(f1);
+verif(f2);
 return (0);
 }
