3c1bc779367529f2c23ac0e1d1f33dc64b9387a0
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-12-21 08:11:29 -0800

    Created hash_table.h, 0-has_tablecreate.c file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+* hash_table_create - function to creato a hash table
+* @size: size of table
+* Return: hash_table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *ht;
+
+if (size < 1)
+return (NULL);
+ht = malloc(sizeof(hash_table_t));
+if (!ht)
+return (NULL);
+ht->size = size;
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (!(ht->array))
+{
+free(ht);
+return (NULL);
+}
+return (ht);
+}
